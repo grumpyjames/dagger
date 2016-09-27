5f7a67e3a72012f4c061ee0ac1cc57fba38439fb
James Byatt <grumpyjames@gmail.com>
2016-09-27 11:15:46 +0100

    expand to a TwoSource

--- src/test/java/net/digihippo/DaggerTest.java
+++ src/test/java/net/digihippo/DaggerTest.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Supplier;
 
+import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static org.junit.Assert.assertEquals;
 
@@ -15,13 +16,38 @@ public class DaggerTest {
     @Test
     public void can_collect_from_a_single_source()
     {
-        final Source<String> src = source(() -> "hello world");
+        final OneSource<String> src = source(() -> "hello world");
         src.consume(output::add);
 
         assertEquals(singletonList("hello world"), output);
     }
 
-    private Source<String> source(Supplier<String> stringSupplier) {
-        return new Source<>(stringSupplier);
+    @Test
+    public void can_map_to_a_different_type()
+    {
+        final OneSource<String> src = source(() -> "hello world");
+        src.map(String::length)
+                .consume(l -> output.add(Long.toString(l)));
+
+        assertEquals(singletonList("11"), output);
+    }
+
+    @Test
+    public void can_map_to_two_different_types()
+    {
+        final OneSource<String> src = source(() -> "hello world");
+        src.mapTwo(String::length, this::firstWord)
+                .consume(l -> output.add(Long.toString(l)), output::add);
+
+        assertEquals(asList("11", "hello"), output);
+    }
+
+    private OneSource<String> source(Supplier<String> stringSupplier) {
+        return new OneSource<>(stringSupplier);
+    }
+
+    private String firstWord(final String input)
+    {
+        return input.split(" ")[0];
     }
 }
